e9d0bb6dc38c8b5d9ffd410b32805e4068150bb3
Ribek <ribekbuddhacharya9999@gmail.com>
2025-07-07 07:06:27 +0545

    update

--- frontend/src/components/Inputs/Input.jsx
+++ frontend/src/components/Inputs/Input.jsx
@@ -14,7 +14,9 @@ const Input = ({ label, value, onChange, placeholder, type }) => {
 
       <div className="input-box">
         <input
-          type={type == 'password' ? showPassword ? 'text' : 'password' : type}
+          type={
+            type == "password" ? (showPassword ? "text" : "password") : type
+          }
           placeholder={placeholder}
           className="w-full bg-transparent outline-none"
           value={value}
